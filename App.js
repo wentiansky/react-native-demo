aa6999603a7bc493b34079aaf4b03f7840a47f81
wentiansky <2087152746@qq.com>
2019-08-26 23:49:51 +0800

    import antd-mobile

--- App.js
+++ App.js
@@ -5,62 +5,25 @@ import {
   View,
   Text,
   Image,
-  TextInput,
-  Alert,
-  Button,
-  FlatList,
-  SectionList
 } from 'react-native';
 
-export default class App extends Component {
-  state = {
-    list: [
-      {
-        title: 'A',
-        data: ['A1', 'A2', 'A3']
-      },
-      {
-        title: 'B',
-        data: ['B1', 'B2', 'B3', 'B4']
-      },
-    ]
-  }
+import { Menu } from 'antd-mobile';
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <SectionList
-          sections={this.state.list}
-          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-      />
-      </View>
-    );
-  }
+const App = () => {
+  return (
+    <View style={styles.container}>
+      {/* <Button>按钮</Button> */}
+      <Menu />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // flexDirection: 'row',
-    // justifyContent: 'center',
-    // alignItems: 'center'
-    paddingTop: 60
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 80
-  },
-  sectionHeader: {
-    padding: 10,
-    // paddingTop: 2,
-    // paddingLeft: 10,
-    // paddingRight: 10,
-    // paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
+    padding: 20,
+    backgroundColor: '#f7f7f7'
+  }
 })
+
+export default App;
